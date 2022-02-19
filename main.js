613b7bbe751a32f4252249c62dd71eb5c253143c
ianniculescu <ian.niculescu@gmail.com>
2022-02-19 12:25:51 +0100

    added map

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const countries = ["Burundi","Suriname","Irak","Iran","Nigeria","Sierra Leone","France","Usa"];
 const button = document.getElementById("button");
 const p = document.getElementById("country");
+let map = L.map('map').setView([39.03961934308089, 125.7586628039601], 7);
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl= 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl,{attribution});
+tiles.addTo(map);
+
 
 
 function getRndInteger(min,max){
